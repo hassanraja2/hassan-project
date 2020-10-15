1f9399168da29a0004f069382319cca16d9b56f0
hassanraja2 <rajahassan42@hotmail.co.uk>
2020-10-15 15:54:47 +0100

    changing dockerfile

--- service-1/build.sh
+++ service-1/build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker build -f /home/jenkins/.jenkins/workspace/project/service-1/Dockerfile -t localhost:8082/service_1  . 
+sudo docker build -f service-1/Dockerfile -t localhost:8082/service_1  . 
