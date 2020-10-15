9a66a1e76de7f40dcd26f9b7beee33613ac82a7e
hassanraja2 <71331663+hassanraja2@users.noreply.github.com>
2020-10-15 10:43:28 +0100

    Update build.sh

--- service-1/build.sh
+++ service-1/build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker build -t localhost:8082/service_1 . /home/jenkins/.jenkins/workpace/project/service-1/
+sudo docker build -f /home/jenkins/.jenkins/workpace/project/service-1/ -t localhost:8082/service_1 . 
