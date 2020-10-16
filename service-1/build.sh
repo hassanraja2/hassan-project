ed4d9e51e15753edf292cad315f60f4a3c57e8ff
hassanraja2 <rajahassan42@hotmail.co.uk>
2020-10-16 10:22:47 +0100

    service-2 changes

--- service-1/build.sh
+++ service-1/build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker build -f service-1/Dockerfile -t localhost:8082/service_1  . 
+sudo docker build -t localhost:8082/service_1  ./service-1 
--- service-2/build.sh
+++ service-2/build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker build -f service-2/Dockerfile -t localhost:8082/service_2 .
+sudo docker build -t localhost:8082/service_2 ./service-2
