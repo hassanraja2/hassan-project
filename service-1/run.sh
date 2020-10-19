2a2f497c71185db72ed5e6e9ad60efb4fc5f61c4
hassanraja2 <rajahassan42@hotmail.co.uk>
2020-10-19 10:40:14 +0100

    app working

--- service-1/run.sh
+++ service-1/run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker run -d -p 5000:5000 --name service_1 --network my_network localhost:8082/service_1 
+sudo docker run -d -p 5000:5000 --name service1 --network my_network localhost:8082/service_1 
--- service-4/run.sh
+++ service-4/run.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo docker run -d -p 5003:5003 --name service_4 --network my_network localhost:8082/service_4
+sudo docker run -d -p 5003:5003 --name service4 --network my_network localhost:8082/service_4
